0dfd3786781acb3668ca1326f9c672dc19dc17a1
Luis Rangel <luisrangelc@gmail.com>
2022-01-08 13:44:06 -0600

    add params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola mundo!!!';
+    // return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return `cons /sas/`;
+  }
+
+  @Get('products/:id')
+  getProduct(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `product ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('id') id: number, @Param('productId') productId: number) {
+    return `category ${id}, products ${productId}`;
   }
 }
