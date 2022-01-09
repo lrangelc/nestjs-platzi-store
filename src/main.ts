fecdb09f519ddce97e450a7ec65b39e2431746aa
Luis Rangel <luisrangelc@gmail.com>
2022-01-08 22:34:07 -0600

    add whitelist

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
   console.log(`http://localhost:${port}`);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port);
 }
 bootstrap();
